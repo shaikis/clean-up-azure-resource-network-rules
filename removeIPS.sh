1996e9c8cbf4f130a40e2075a327a70a5b2dad81
Ismail Shaik <60819791+shaikis@users.noreply.github.com>
2021-09-30 06:56:29 +0530

    Update removeIPS.sh

--- removeIPS.sh
+++ removeIPS.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 # login your azure account
-az login --tenant 
+az login --tenant <tenant id> # this is not required if you build from azure devops pipeline az cli
+az account set --subscription <subscription id>  # this is not required if you build from azure devops pipeline az cli
 
 # clean up storage accounts.
 while read -r group;
 do
  resource_group_name="echo $line |awk '{print $1}'"
- storage_account_name="echo $group |awk '{print $2}'"
+ resource_name="echo $group |awk '{print $2}'"
  resource_type="echo $group |awk '{print $3}'"
 
  if [ "${resource_type}" == "storage"];
  then
-    command="az storage account network-rule list --resource-group ${resource_group_name} --account-name ${storage_account_name} --query ipRules"
+    command="az storage account network-rule list --resource-group ${resource_group_name} --account-name ${resource_name} --query ipRules"
  elif [ "${resource_type}" == "vault"]; 
  then
-    command="az keyvault network-rule"
+    command="az keyvault network-rule list --name ${resource_name} --resource-group ${resource_group_name}"
  elif [ "${resource_type}" == "SQLserver"]; 
  then
-    command = ""
+    command = "az sql server firewall-rule list -g ${resource_group} -s ${resource_name}"
  elif [ "${resource_type}" == "Synapse"]; 
  then
     command = ""
@@ -34,13 +35,13 @@ do
      ip_add="echo $ip |awk '{print $1}' "
       if [ "${resource_type}" == "storage"];
       then
-       command="az storage account network-rule list --resource-group ${resource_group_name} --account-name ${storage_account_name} --query ipRules"
+       command="az storage account network-rule remove --resource-group ${resource_group_name} --account-name ${resource_name} --ip-address ${ip_add}"
       elif [ "${resource_type}" == "vault"]; 
       then
-       command="az keyvault network-rule"
+       command="az keyvault network-rule remove --name ${resource_name} --ip-address ${ip_add}/32"
       elif [ "${resource_type}" == "SQLserver"]; 
       then
-       command = ""
+       command = "az sql server firewall-rule delete"
       elif [ "${resource_type}" == "Synapse"]; 
       then
        command = ""
@@ -49,7 +50,8 @@ do
       fi
     
     # remove the IP's from resource.
-     ${command} remove --resource-group ${resource_group_name} --account-name $storage_account_name --ip-address ${ip_add}
+     ${command}
+    #
      done < storage_ips.txt
 
 done < resource.txt   
